bd87763552beaf0ebf3d5f2efa7342490f935373
Thomas Chapeaux <tom@wooclap.com>
2022-06-25 14:30:51 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import "./App.css";
+
+import PRECEPTS from "../assets/precepts.json";
+import { useState } from "react";
+
+function App() {
+  const [selected, setSelected] = useState(0);
+
+  const precept = PRECEPTS[selected];
+
+  function setRandom() {
+    const r = Math.round(Math.random() * (PRECEPTS.length - 1));
+    setSelected(r);
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>PRECEPTS OF ZOTE</h1>
+        <blockquote>
+          To what do I owe my long string of trimphs and victories, you ask?
+          [T]he key is in my personal philosophy, the 'Fifty-Seven Precepts of
+          Zote'
+        </blockquote>
+      </header>
+      <nav className="choose-precept">
+        {PRECEPTS.map((p) => (
+          <button
+            className={p.idx === selected ? "selected" : undefined}
+            key={p.idx}
+            onClick={() => setSelected(p.idx)}
+          >
+            {p.idx + 1}
+          </button>
+        ))}
+        <button onClick={setRandom}>🎲</button>
+      </nav>
+      <main className="one-precept">
+        <p className="prcpt-nbr">{precept.nbr}</p>
+        <p className="prcpt-title">{precept.title}</p>
+        <p className="prcpt-text">{precept.text}</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
